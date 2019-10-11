ff327a992ea3b2c3fb51f5265c74e9e7f605641f
tbhc <43885015+tbhc@users.noreply.github.com>
2019-10-11 15:13:31 +0300

    Update euler.cpp

--- euler.cpp
+++ euler.cpp
@@ -2,59 +2,52 @@
 #include <list> 
 using namespace std;
 
-class Graph
-{
+class Graph {
 	int V;
 	list<int>* adj;
 public:
-	Graph(int V)
-	{
+	Graph(int V) {
 		this->V = V;
 		adj = new list<int>[V];
 	}
-	~Graph()
-	{
+	
+	~Graph() {
 		delete[] adj;
 	}
 
 	void addEdge(int v, int w);
 };
 
-void Graph::addEdge(int v, int w)
-{
+void Graph::addEdge(int v, int w) {
 	adj[v].push_back(w);
 	adj[w].push_back(v);
 }
 
-int main()
-{
+int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	int N, M;
 	cin >> N >> M;
 	int u[100], v[100], counter[100];
+	
 	Graph g(N);
-	for (int i = 0; i < M; i++)
-	{
+	for (int i = 0; i < M; i++) {
 		cin >> u[i] >> v[i];
 		g.addEdge(u[i], v[i]);
 	}
-	for (int i = 0; i < N; i++)
-	{
+	for (int i = 0; i < N; i++) {
 		counter[i] = 0;
-		for (int j = 0; j < M; j++)
-		{
+		for (int j = 0; j < M; j++) {
 			if (u[j] == i or v[j] == i)
 				counter[i]++;
 		}
 	}
+	
 	int result = 0;
 	int min = -1;
 	int max = -1;
-	for (int j = 0; j < N; j++)
-	{
-		if (counter[j] % 2 != 0)
-		{
+	for (int j = 0; j < N; j++) {
+		if (counter[j] % 2 != 0) {
 			result++;
 			if (min == max)
 				min = j;
